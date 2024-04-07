9be812cba1739aa24599d735aad61b7bd8aa8e95
niranjan <niranjanchougule17@gmail.com>
2024-04-07 20:23:43 +0530

    infinite scrolling sample

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [total, setTotal] = useState();
+
+  const fetchProducts = async () => {
+    const res = await fetch(
+      `https://dummyjson.com/products?skip=${page * 10}&limit=10`
+    );
+    const data = await res.json();
+
+    console.log(data);
+    setProducts((prevProducts) => [...prevProducts, ...data.products]);
+    setPage((prevPage) => prevPage + 1);
+    setLoading(false);
+    setTotal(data.total);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    console.log("useEffect called");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="product-list">
+      <InfiniteScroll
+        dataLength={products.length}
+        next={fetchProducts}
+        hasMore={total === page * 10 ? false : true}
+        endMessage={"End of the list"}
+        loader={<h4>Loading...</h4>}
+        scrollThreshold={0.9}
+      >
+        {products.map((prod) => {
+          return (
+            <span className="product-box" key={prod.id}>
+              <img src={prod.thumbnail} alt="product imag" />
+              <span>{prod.title}</span>
+            </span>
+          );
+        })}
+      </InfiniteScroll>
+      {/* {loading && <div>Loading...</div>} */}
     </div>
   );
 }
